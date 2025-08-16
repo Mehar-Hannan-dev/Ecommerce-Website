34b53ccde7b116771e49140d8dc78de76e19d5ff
Mehar Hannan <meharhannan.dev@gmail.com>
2025-08-16 23:55:16 +0500

    .

--- ecommerce_website/lib/website/pages/Home_Page.dart
+++ ecommerce_website/lib/website/pages/Home_Page.dart
@@ -17,42 +17,58 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: [
           Column(children: []),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ResponsiveText(text: 'text'),
-              Row(
-                children: [
-                  ResponsiveText(text: 'text'),
-                  ResponsiveText(text: 'text'),
-                  ResponsiveText(text: 'text'),
-                ],
-              ),
-              Row(
-                children: [
-                  ResponsiveIcon(
-                    icon: Icons.search_rounded,
-                    size: const ResponsiveValue(
-                      mobile: 16,
-                      desktop: 24,
-                      tablet: 20,
+          CustomPadding(
+            top: 20,
+            left: 20,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ResponsiveText(
+                  text: 'CoCo',
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: context.primaryText,
+                  ),
+                  mobileFontSize: 16,
+                  desktopFontSize: 24,
+                  tabletFontSize: 20,
+                  mobileLandscapeFontSize: 18,
+                  tabletLandscapeFontSize: 22,
+                ),
+                Row(
+                  children: [
+                    ResponsiveText(text: 'text'),
+                    ResponsiveText(text: 'text'),
+                    ResponsiveText(text: 'text'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ResponsiveIcon(
+                      icon: Icons.search_rounded,
+                      size: const ResponsiveValue(
+                        mobile: 16,
+                        desktop: 24,
+                        tablet: 20,
+                      ),
                     ),
-                  ),
-                  ResponsiveIcon(
-                    icon: Icons.shopping_bag_rounded,
-                    size: const ResponsiveValue(
-                      mobile: 16,
-                      desktop: 24,
-                      tablet: 20,
+                    ResponsiveIcon(
+                      icon: Icons.shopping_bag_rounded,
+                      size: const ResponsiveValue(
+                        mobile: 16,
+                        desktop: 24,
+                        tablet: 20,
+                      ),
                     ),
-                  ),
-                  ResponsiveButton(
-                    child: ResponsiveText(text: 'text'),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ],
+                    ResponsiveButton(
+                      child: ResponsiveText(text: 'text'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
